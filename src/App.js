9a719e4ba19c45d05127fcda6a8caad0616bb3df
Aaron <aaron1venema@gmail.com>
2023-02-14 14:29:23 -0600

    tutorial project 4

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import data from './data';
+import SingleQuestion from './Question';
+console.log(data)
+
+
+function App() {
+
+
+  return (
+    <main>
+      <div className="container">
+        <h3>Questions and answers about login</h3>
+        <section className='info'>
+           {data.map((question) => {
+            return <SingleQuestion key={question.id} {...question}/>
+          })}
+        </section>
+      </div>
+    </main>
+  );
+}
+
+export default App;
